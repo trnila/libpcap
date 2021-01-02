4c529def9490b06755442896e8fb1cdd0999c0c9
Daniel Trnka <daniel.trnka@gmail.com>
2021-01-02 13:34:17 +0100

    pcap-rpmsg: Support mmap

--- pcap-rpmsg.c
+++ pcap-rpmsg.c
@@ -25,8 +25,11 @@
 #include <sys/mman.h>
 #include <sys/utsname.h>
 
+#define VEC_SIZE 32
+
 struct pcap_rpmsg {
-  int fd;
+  char *mmap_buf;
+  size_t mmap_buflen;
 };
 
 struct rpmsg_header {
@@ -41,9 +44,97 @@ struct rpmsg_mon_header {
   uint64_t timestamp;
   uint32_t interface;
   uint16_t vq;
-  uint16_t len;
+  uint16_t res;
   struct rpmsg_header hdr;
 };
+
+struct mon_bin_mfetch {
+	int32_t *offvec;   /* Vector of events fetched */
+	int32_t nfetch;    /* Number of events to fetch (out: fetched) */
+	int32_t nflush;    /* Number of events to flush */
+};
+
+#define MON_IOC_MAGIC 0x92
+
+#define MON_IOCX_MFETCH _IOWR(MON_IOC_MAGIC, 7, struct mon_bin_mfetch)
+#define MON_IOCH_MFLUSH _IO(MON_IOC_MAGIC, 8)
+
+static int
+rpmsg_read_mmap(pcap_t *handle, int max_packets _U_, pcap_handler callback, u_char *user) {
+	struct pcap_rpmsg *handlep = handle->priv;
+	struct mon_bin_mfetch fetch;
+	int32_t vec[VEC_SIZE];
+	struct pcap_pkthdr pkth;
+	struct rpmsg_mon_header* hdr;
+	int nflush = 0;
+	int packets = 0;
+	u_int clen, max_clen;
+
+	max_clen = handle->snapshot - sizeof(pcap_usb_header_mmapped);
+
+	for (;;) {
+		int i, ret;
+		int limit = max_packets - packets;
+		if (limit <= 0)
+			limit = VEC_SIZE;
+		if (limit > VEC_SIZE)
+			limit = VEC_SIZE;
+
+		/* try to fetch as many events as possible*/
+		fetch.offvec = vec;
+		fetch.nfetch = limit;
+		fetch.nflush = nflush;
+		/* ignore interrupt system call errors */
+		do {
+			ret = ioctl(handle->fd, MON_IOCX_MFETCH, &fetch);
+			if (handle->break_loop)
+			{
+				handle->break_loop = 0;
+				return -2;
+			}
+		} while ((ret == -1) && (errno == EINTR));
+		if (ret < 0)
+		{
+			if (errno == EAGAIN)
+				return 0;	/* no data there */
+
+			pcap_fmt_errmsg_for_errno(handle->errbuf,
+			    PCAP_ERRBUF_SIZE, errno, "Can't mfetch fd %d",
+			    handle->fd);
+			return -1;
+		}
+
+		/* keep track of processed events, we will flush them later */
+		nflush = fetch.nfetch;
+		for (i=0; i<fetch.nfetch; ++i) {
+			/* discard filler */
+			hdr = (struct rpmsg_mon_header*) &handlep->mmap_buf[vec[i]];
+      pkth.caplen = pkth.len = hdr->hdr.length + sizeof(struct rpmsg_mon_header);;
+      pkth.ts.tv_sec = hdr->timestamp / 1000000000UL;
+      pkth.ts.tv_usec = hdr->timestamp % 1000000000UL;
+
+			if (handle->fcode.bf_insns == NULL ||
+			    pcap_filter(handle->fcode.bf_insns, (u_char*) hdr,
+			      pkth.len, pkth.caplen)) {
+//				handlep->packets_read++;
+				callback(user, &pkth, (u_char*) hdr);
+				packets++;
+			}
+		}
+
+		/* with max_packets specifying "unlimited" we stop afer the first chunk*/
+		if (PACKET_COUNT_IS_UNLIMITED(max_packets) || (packets == max_packets))
+			break;
+	}
+
+	/* flush pending events*/
+	if (ioctl(handle->fd, MON_IOCH_MFLUSH, nflush) == -1) {
+		pcap_fmt_errmsg_for_errno(handle->errbuf, PCAP_ERRBUF_SIZE,
+		    errno, "Can't mflush fd %d", handle->fd);
+		return -1;
+	}
+	return packets;
+}
 
 static int
 rpmsg_read(pcap_t *handle, int max_packets _U_, pcap_handler callback, u_char *user) {
@@ -102,6 +193,15 @@ static int rpmsg_activate(pcap_t* handle) {
 	handle->getnonblock_op = pcap_getnonblock_fd;
 	handle->setnonblock_op = pcap_setnonblock_fd;
 	handle->buffer = malloc(handle->bufsize);
+
+  // TODO: buffer size!
+  priv->mmap_buflen = 1024 * 1024 * 1024;
+  priv->mmap_buf = mmap(0, priv->mmap_buflen, PROT_READ, MAP_SHARED, handle->fd, 0);
+  if(priv->mmap_buf == MAP_FAILED) {
+    perror("map failed\n");
+  }
+  handle->read_op = rpmsg_read_mmap;
+
 	if (!handle->buffer) {
 		pcap_fmt_errmsg_for_errno(handle->errbuf, PCAP_ERRBUF_SIZE,
 		    errno, "malloc");
